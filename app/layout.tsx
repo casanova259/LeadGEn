2760b778f614660baec8560fcb7bcd6be0f44d1f
Manik Sharma <casanova270407@gmail.com>
2026-07-23 13:45:08 +0530

    did it

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
-      <html lang="en"><body>{children}</body></html>
+      <html lang="en" className="dark"><body>{children}</body></html>
     </ClerkProvider>
   )
 }
